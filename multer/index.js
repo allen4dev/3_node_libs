0235237e016662c7497f9b3e289d617330432784
allen4dev <alanaliagadev@gmail.com>
2017-11-21 19:59:01 -0400

    [Add] multer: DiskStorage

--- multer/index.js
+++ multer/index.js
@@ -1,15 +1,42 @@
 const http = require('http');
 const express = require('express');
 const multer = require('multer');
+const uuid = require('uuid');
+const ext = require('file-extension');
 
 const app = express();
+
+const pathToUpload = `${__dirname}/public/uploads`;
+
+function isPhoto(file) {
+  return file.mimetype.startsWith('image/');
+}
+
+function createFilename(file) {
+  const extension = ext(file.originalname);
+  return `${uuid()}.${extension}`;
+}
+
+const diskStorage = multer.diskStorage({
+  destination: function(req, file, next) {
+    if (!isPhoto(file))
+      return next({ message: "That filetype isn't allowed!" }, false);
+
+    next(null, pathToUpload);
+  },
+  filename: function(req, file, next) {
+    next(null, createFilename(file));
+  },
+});
+
+const upload = multer({ storage: diskStorage });
 
 app.get('/', (req, res, next) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.post('/uploadImage', (req, res, next) => {
-  console.log('Upload image here');
+app.post('/uploadImage', upload.single('image'), (req, res, next) => {
+  console.log(req.file);
   res.redirect('/');
 });
 
