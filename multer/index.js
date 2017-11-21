92f4fec4c19cf846d40692dd1f54bad7acfd0bdf
allen4dev <alanaliagadev@gmail.com>
2017-11-21 19:48:31 -0400

    first commit

---
+++ multer/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.post('/uploadImage', (req, res, next) => {
+  console.log('Upload image here');
+  res.redirect('/');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ error: err });
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, console.log('Server running in port 3000'));
