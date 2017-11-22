93beba157e64b11b46c441df783ebc27462f4b14
allen4dev <alanaliagadev@gmail.com>
2017-11-21 20:12:54 -0400

    [Add] multer: MemoryStorage, jimp: Basic image resize

--- multer/index.js
+++ multer/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const multer = require('multer');
 const uuid = require('uuid');
+const jimp = require('jimp');
 const ext = require('file-extension');
 
 const app = express();
@@ -17,6 +18,7 @@ function createFilename(file) {
   return `${uuid()}.${extension}`;
 }
 
+// DiskStorage
 const diskStorage = multer.diskStorage({
   destination: function(req, file, next) {
     if (!isPhoto(file))
@@ -29,13 +31,39 @@ const diskStorage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: diskStorage });
+// const upload = multer({ storage: diskStorage });
+
+// MemoryStorage
+function fileFilter(req, file, next) {
+  if (!isPhoto(file))
+    return next({ message: "That filetype isn't allowed!" }, false);
+
+  next(null, true);
+}
+
+const upload = multer({ storage: multer.memoryStorage(), fileFilter });
+
+// Resize
+function resize(req, res, next) {
+  if (!req.file) return next(new Error('No file passed'));
+
+  jimp
+    .read(req.file.buffer)
+    .then(photo => {
+      return photo.resize(400, jimp.AUTO);
+    })
+    .then(resized => {
+      return resized.write(`${pathToUpload}/${createFilename(req.file)}`);
+    })
+    .then(() => next())
+    .catch(next);
+}
 
 app.get('/', (req, res, next) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.post('/uploadImage', upload.single('image'), (req, res, next) => {
+app.post('/uploadImage', upload.single('image'), resize, (req, res, next) => {
   console.log(req.file);
   res.redirect('/');
 });
