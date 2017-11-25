12636a726cb5451cbae2a717edf579f5b41c8252
allen4dev <alanaliagadev@gmail.com>
2017-11-25 11:43:55 -0400

    [Add] flash messages

--- 3_dotenv_passport-mongoose_flash-messsages/server/app.js
+++ 3_dotenv_passport-mongoose_flash-messsages/server/app.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const flash = require('connect-flash');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/libs-test');
@@ -12,9 +14,11 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser('keyboard cat'));
 app.use(session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 passport.use(User.createStrategy());
 
@@ -22,7 +26,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
-  res.json({ test: process.env.TEST });
+  res.json({ flash: req.flash() });
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.post('/register', (req, res, next) => {
@@ -32,7 +40,8 @@ app.post('/register', (req, res, next) => {
   User.register(user, password, (err, user) => {
     if (err) return next(err);
     if (!user) return next(new Error('Not found'));
-    res.status(200).send({ user });
+    req.flash('success', 'User successfully created');
+    res.redirect('/');
   });
 });
 
