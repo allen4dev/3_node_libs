cd4224e3f33c1933366257cba11750118c240938
allen4dev <alanaliagadev@gmail.com>
2017-12-06 17:38:38 -0400

    [Add] socketio: docs chat example

---
+++ 6_socketio/docs/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', socket => {
+  console.log('A user connected');
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => console.log('Server running in port 3000'));
